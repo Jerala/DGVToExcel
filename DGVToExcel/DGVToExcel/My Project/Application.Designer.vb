29572ec1e0fbb57160866b03b0a3bc59df013f65
Jerala <jerala@mail.ru>
2018-04-01 14:48:35 +0300

    -

--- DGVToExcel/DGVToExcel/My Project/Application.Designer.vb
+++ DGVToExcel/DGVToExcel/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.DGVToExcel.Form1
+            Me.MainForm = Global.DGVToExcel.Export_to_excel
         End Sub
     End Class
 End Namespace
